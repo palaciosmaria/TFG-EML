0567b422d31222da587ab9a458abbe140f1cf0cc
palaciosmaria <m.palacios14@usp.ceu.es>
2021-05-19 21:37:25 +0200

    FILES WITH OTHER 2 DATABASES

--- logistic_l1_diabetes2.py
+++ logistic_l1_diabetes2.py
@@ -2,55 +2,29 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Apr 20 11:05:43 2021
+
 @author: mariapalacios
 """
 
 import numpy as np
 import pandas as pd
-from sklearn.impute import SimpleImputer
 from sklearn.linear_model import LogisticRegression
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import classification_report
-from sklearn.model_selection import train_test_split
 
 RANDOM_SEED=42
 
 # No uses rutas absolutas (solo funcionan en tu ordenador), usa rutas relativas
-dataset = pd.read_csv("datasets/healthcare-dataset-stroke-data.csv")
+dataset = pd.read_csv("datasets/diabetes2.csv")
 dataset.head()
 
-# Let's give better names to some categorical levels
-dataset['hypertension'] = dataset['hypertension'].replace([0, 1], ['healthy-tension', 'hypertense'])
-dataset['heart_disease'] = dataset['heart_disease'].replace([0, 1], ['healthy-heart', 'heart-disease'])
-dataset['ever_married'] = dataset['ever_married'].replace(['No', 'Yes'], ['not-ever-married', 'ever-married'])
-dataset['smoking_status'] = dataset['smoking_status'].replace(['Unknown'], ['unknown'])
-
-X = dataset.drop(['id'], axis=1).iloc[:, :-1]
+X = dataset.iloc[:, :-1]
+X_features = X.columns
 y = dataset.iloc[:, -1].values
-# print(np.unique(y, return_counts=True)) # count number of 0s and 1s
 
-# Remove 'Other' gender for easining the interpretation of results 
-dataset = dataset.loc[dataset['gender'] != 'Other', :]
+X = StandardScaler().fit_transform(X)
 
-# Careful, this should be reviewed for each dataset
-numerical_idx = X.select_dtypes(include=['float64']).columns
-categorical_idx = X.select_dtypes(include=['object', 'bool', 'int64']).columns
-assert X.shape[1] == (len(numerical_idx) + len(categorical_idx)), 'some column is missing'
-
-# impute BMI's NAs
-# X.columns[X.isna().any()].tolist() # This line may be used to check that indeed BMI has NAs
-numerical_imp = SimpleImputer()
-X_numerical = numerical_imp.fit_transform(X[numerical_idx])
-
-# Encode categorical variables
-oh_enc = OneHotEncoder()
-X_cat = oh_enc.fit_transform(X[categorical_idx]).todense()
-
-X = np.concatenate([X_cat, X_numerical], axis=1)
-X_features = np.concatenate([oh_enc.get_feature_names(), numerical_idx])
-
-
-C = 0.001
+C=0.015
 est = LogisticRegression(penalty='l1', C=C, random_state=RANDOM_SEED, solver='saga', max_iter=1000,
                          class_weight='balanced')
 est.fit(X, y)
@@ -60,18 +34,11 @@ sparsity_l1 = np.mean(coefs == 0) * 100
 print(f"C={C}")
 print(f"Sparsity with L1 penalty: {sparsity_l1} ({np.sum(coefs != 0)} / {len(coefs)} columns)")
 print(f"Relevant columns are: {', '.join(relevant_columns)}")
-print('Classification summary:')
 print(classification_report(y, est.predict(X)))
-# print('Classification summary:')
-# print(classification_report(y, est.predict(X)))
-
-# El clasificador base solo usa age, avg_glucose_level, bmi
-# Busca explicaciones en las que aparezcan variables distintas a estas tres.
-# Usa X e y como datos (los nombres de las columnas estan en X_features), est  es el clasificador final
 
 import lime.lime_tabular
 #from lime.lime_text import LimeTabularExplainer
-class_names=['healthy','stroke']
+class_names=['healthy','heart_failure']
 X= np.asarray(X)
 explainer = lime.lime_tabular.LimeTabularExplainer(X, feature_names = list(X_features), 
                                                   class_names=class_names,
@@ -93,12 +60,12 @@ counter_columns_correct_predictions=0
 counter_columns_incorrect_predictions=0
 
 correct_columns=0
-prediction='healthy'
+prediction='diabetes'
 counter_correct_predictions=0
 counter_incorrect_predictions=0
 
 for i in range(len(dataset)):
-    exp = explainer.explain_instance(X[i], est.predict_proba, num_features=3, top_labels=len(X_features))#num features es tres
+    exp = explainer.explain_instance(X[i], est.predict_proba, num_features=2, top_labels=len(X_features))#num features es tres
     #porque es realemente lo que queremos, lo que se sabemos que son importantes.
     explanations = [explanation for (explanation, _) in exp.as_list()]
 
@@ -107,14 +74,14 @@ for i in range(len(dataset)):
 
     correct_prediction=True
     if (est.predict_proba([X[i]])[0, 1] ) >= 0.5:
-        prediction='stroke'
+        prediction='diabetes'
     else:
-        prediction='healthy'
+        prediction='no_diabetes'
         
-    if (prediction=='healthy') and (class_names[y[i]]=='healthy'):
+    if (prediction=='no_diabetes') and (class_names[y[i]]=='no_diabetes'):
         correct_prediction=True
         counter_correct_predictions=counter_correct_predictions+1
-    elif (prediction=='stroke') and (class_names[y[i]]=='stroke'):
+    elif (prediction=='diabetes') and (class_names[y[i]]=='diabetes'):
         counter_correct_predictions=counter_correct_predictions+1
         correct_prediction=True
     else:
@@ -154,11 +121,11 @@ for i in range(len(dataset)):
     missing_columns = relevant_columns[np.where(are_columns_present == False)[0]]
     print("*************************************************************")
     print('Document id: %d' % i)
-    print('Probability(stroke) =', est.predict_proba([X[i]])[0, 1])
+    print('Probability(diabetes) =', est.predict_proba([X[i]])[0, 1])
     print('True class: %s' % class_names[y[i]])
-    if (prediction=='healthy') and (class_names[y[i]]=='healthy'):
+    if (prediction=='no_diabetes') and (class_names[y[i]]=='no_diabetes'):
         print('Prediction is correct')
-    elif (prediction=='stroke') and (class_names[y[i]]=='stroke'):
+    elif (prediction=='diabetes') and (class_names[y[i]]=='diabetes'):
         print('Prediction is correct')
     else:
         print('Prediction is wrong')
@@ -181,10 +148,3 @@ print(counter_columns_correct_predictions/(counter_correct_predictions*3))
 print('incorrect prediction')
 print(correct_explanations_incorrect_predictions/counter_incorrect_predictions)
 print(counter_columns_incorrect_predictions/(counter_incorrect_predictions*3))
-
-
-
-
-
-##encontrar ejemplos de explicaciones razonables en predicciones incorrectas
-
--- logistic_l1_heart_failure.py
+++ logistic_l1_heart_failure.py
@@ -2,55 +2,49 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Apr 20 11:05:43 2021
+
 @author: mariapalacios
 """
 
 import numpy as np
 import pandas as pd
-from sklearn.impute import SimpleImputer
 from sklearn.linear_model import LogisticRegression
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import classification_report
-from sklearn.model_selection import train_test_split
+
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder
 
 RANDOM_SEED=42
 
 # No uses rutas absolutas (solo funcionan en tu ordenador), usa rutas relativas
-dataset = pd.read_csv("datasets/healthcare-dataset-stroke-data.csv")
+dataset = pd.read_csv("datasets/heart_failure_clinical_records_dataset.csv")
 dataset.head()
 
-# Let's give better names to some categorical levels
-dataset['hypertension'] = dataset['hypertension'].replace([0, 1], ['healthy-tension', 'hypertense'])
-dataset['heart_disease'] = dataset['heart_disease'].replace([0, 1], ['healthy-heart', 'heart-disease'])
-dataset['ever_married'] = dataset['ever_married'].replace(['No', 'Yes'], ['not-ever-married', 'ever-married'])
-dataset['smoking_status'] = dataset['smoking_status'].replace(['Unknown'], ['unknown'])
+dataset['anaemia'] = dataset['anaemia'].replace([0, 1], ['not-anaemia', 'anaemia'])
+dataset['diabetes'] = dataset['diabetes'].replace([0, 1], ['not-diabetes', 'diabetes'])
+dataset['high_blood_pressure'] = dataset['high_blood_pressure'].replace([0, 1], ['normal-bp', 'high-bp'])
+dataset['sex'] = dataset['sex'].replace([0, 1], ['Female', 'Male'])
+dataset['smoking'] = dataset['smoking'].replace([0, 1], ['not-smoker', 'smoker'])
 
-X = dataset.drop(['id'], axis=1).iloc[:, :-1]
+
+X = dataset.drop('time', axis=1).iloc[:, :-1]
+X_features = X.columns
 y = dataset.iloc[:, -1].values
-# print(np.unique(y, return_counts=True)) # count number of 0s and 1s
 
-# Remove 'Other' gender for easining the interpretation of results 
-dataset = dataset.loc[dataset['gender'] != 'Other', :]
-
-# Careful, this should be reviewed for each dataset
-numerical_idx = X.select_dtypes(include=['float64']).columns
-categorical_idx = X.select_dtypes(include=['object', 'bool', 'int64']).columns
+numerical_idx = X.select_dtypes(include=['float64', 'int64']).columns
+categorical_idx = X.select_dtypes(include=['object', 'bool']).columns
 assert X.shape[1] == (len(numerical_idx) + len(categorical_idx)), 'some column is missing'
 
-# impute BMI's NAs
-# X.columns[X.isna().any()].tolist() # This line may be used to check that indeed BMI has NAs
-numerical_imp = SimpleImputer()
-X_numerical = numerical_imp.fit_transform(X[numerical_idx])
-
 # Encode categorical variables
+X_numerical = StandardScaler().fit_transform(X[numerical_idx])
 oh_enc = OneHotEncoder()
 X_cat = oh_enc.fit_transform(X[categorical_idx]).todense()
 
 X = np.concatenate([X_cat, X_numerical], axis=1)
 X_features = np.concatenate([oh_enc.get_feature_names(), numerical_idx])
 
-
-C = 0.001
+C = 0.025
 est = LogisticRegression(penalty='l1', C=C, random_state=RANDOM_SEED, solver='saga', max_iter=1000,
                          class_weight='balanced')
 est.fit(X, y)
@@ -62,16 +56,11 @@ print(f"Sparsity with L1 penalty: {sparsity_l1} ({np.sum(coefs != 0)} / {len(coe
 print(f"Relevant columns are: {', '.join(relevant_columns)}")
 print('Classification summary:')
 print(classification_report(y, est.predict(X)))
-# print('Classification summary:')
-# print(classification_report(y, est.predict(X)))
 
-# El clasificador base solo usa age, avg_glucose_level, bmi
-# Busca explicaciones en las que aparezcan variables distintas a estas tres.
-# Usa X e y como datos (los nombres de las columnas estan en X_features), est  es el clasificador final
 
 import lime.lime_tabular
 #from lime.lime_text import LimeTabularExplainer
-class_names=['healthy','stroke']
+class_names=['healthy','heart_failure']
 X= np.asarray(X)
 explainer = lime.lime_tabular.LimeTabularExplainer(X, feature_names = list(X_features), 
                                                   class_names=class_names,
@@ -98,7 +87,7 @@ counter_correct_predictions=0
 counter_incorrect_predictions=0
 
 for i in range(len(dataset)):
-    exp = explainer.explain_instance(X[i], est.predict_proba, num_features=3, top_labels=len(X_features))#num features es tres
+    exp = explainer.explain_instance(X[i], est.predict_proba, num_features=2, top_labels=len(X_features))#num features es tres
     #porque es realemente lo que queremos, lo que se sabemos que son importantes.
     explanations = [explanation for (explanation, _) in exp.as_list()]
 
@@ -107,14 +96,14 @@ for i in range(len(dataset)):
 
     correct_prediction=True
     if (est.predict_proba([X[i]])[0, 1] ) >= 0.5:
-        prediction='stroke'
+        prediction='heart_failure'
     else:
         prediction='healthy'
         
     if (prediction=='healthy') and (class_names[y[i]]=='healthy'):
         correct_prediction=True
         counter_correct_predictions=counter_correct_predictions+1
-    elif (prediction=='stroke') and (class_names[y[i]]=='stroke'):
+    elif (prediction=='heart_failure') and (class_names[y[i]]=='heart_failure'):
         counter_correct_predictions=counter_correct_predictions+1
         correct_prediction=True
     else:
@@ -142,7 +131,7 @@ for i in range(len(dataset)):
         counter_columns_incorrect_predictions+=columns_correctly_retrieved
         
     
-    if columns_correctly_retrieved == 3:
+    if columns_correctly_retrieved == 2:
         correct_explanations=correct_explanations+1
         if correct_prediction:
             correct_explanations_correct_predictions+=1
@@ -154,11 +143,11 @@ for i in range(len(dataset)):
     missing_columns = relevant_columns[np.where(are_columns_present == False)[0]]
     print("*************************************************************")
     print('Document id: %d' % i)
-    print('Probability(stroke) =', est.predict_proba([X[i]])[0, 1])
+    print('Probability(heart_failure) =', est.predict_proba([X[i]])[0, 1])
     print('True class: %s' % class_names[y[i]])
     if (prediction=='healthy') and (class_names[y[i]]=='healthy'):
         print('Prediction is correct')
-    elif (prediction=='stroke') and (class_names[y[i]]=='stroke'):
+    elif (prediction=='heart_failure') and (class_names[y[i]]=='heart_failure'):
         print('Prediction is correct')
     else:
         print('Prediction is wrong')
@@ -183,8 +172,4 @@ print(correct_explanations_incorrect_predictions/counter_incorrect_predictions)
 print(counter_columns_incorrect_predictions/(counter_incorrect_predictions*3))
 
 
-
-
-
-##encontrar ejemplos de explicaciones razonables en predicciones incorrectas
 
