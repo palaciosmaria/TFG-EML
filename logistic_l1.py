58b87e9ff053cde205b3fe8cf665ef146ce7e87d
palaciosmaria <m.palacios14@usp.ceu.es>
2021-05-27 13:11:12 +0200

    shap algorithm

--- logistic_l1.py
+++ logistic_l1.py
@@ -97,93 +97,94 @@ prediction='healthy'
 counter_correct_predictions=0
 counter_incorrect_predictions=0
 
-for i in range(len(dataset)):
-    exp = explainer.explain_instance(X[i], est.predict_proba, num_features=3, top_labels=len(X_features))#num features es tres
-    #porque es realemente lo que queremos, lo que se sabemos que son importantes.
-    explanations = [explanation for (explanation, _) in exp.as_list()]
+i=234
+#for i in range(len(dataset)):
+exp = explainer.explain_instance(X[i], est.predict_proba, num_features=3, top_labels=len(X_features))#num features es tres
+#porque es realemente lo que queremos, lo que se sabemos que son importantes.
+explanations = [explanation for (explanation, _) in exp.as_list()]
 
+
+ #prediction de tener un stroke es true (stroke) si su probabilidad es >50%
+
+correct_prediction=True
+if (est.predict_proba([X[i]])[0, 1] ) >= 0.5:
+    prediction='stroke'
+else:
+    prediction='healthy'
     
-     #prediction de tener un stroke es true (stroke) si su probabilidad es >50%
-
+if (prediction=='healthy') and (class_names[y[i]]=='healthy'):
     correct_prediction=True
-    if (est.predict_proba([X[i]])[0, 1] ) >= 0.5:
-        prediction='stroke'
-    else:
-        prediction='healthy'
-        
-    if (prediction=='healthy') and (class_names[y[i]]=='healthy'):
-        correct_prediction=True
-        counter_correct_predictions=counter_correct_predictions+1
-    elif (prediction=='stroke') and (class_names[y[i]]=='stroke'):
-        counter_correct_predictions=counter_correct_predictions+1
-        correct_prediction=True
-    else:
-        correct_prediction=False
-        counter_incorrect_predictions=counter_incorrect_predictions+1
+    counter_correct_predictions=counter_correct_predictions+1
+elif (prediction=='stroke') and (class_names[y[i]]=='stroke'):
+    counter_correct_predictions=counter_correct_predictions+1
+    correct_prediction=True
+else:
+    correct_prediction=False
+    counter_incorrect_predictions=counter_incorrect_predictions+1
 
-    #while (correct_prediction!=True):
-    are_columns_present = []
-    for column_name in relevant_columns:
-            are_columns_present.append(
-                is_column_in_explanations(column_name, explanations)
-            )
-       
+#while (correct_prediction!=True):
+are_columns_present = []
+for column_name in relevant_columns:
+        are_columns_present.append(
+            is_column_in_explanations(column_name, explanations)
+        )
+   
+
+# convert to array to permit vectorial operations
+are_columns_present = np.array(are_columns_present)
+columns_correctly_retrieved = np.sum(are_columns_present)
+
+ #columns correctly retrieved son las veces que estan bmi, average glucose level y age en las explicaciones
+correct_columns=correct_columns+columns_correctly_retrieved
+if correct_prediction:
+    counter_columns_correct_predictions+=columns_correctly_retrieved
     
-    # convert to array to permit vectorial operations
-    are_columns_present = np.array(are_columns_present)
-    columns_correctly_retrieved = np.sum(are_columns_present)
+else:
+    counter_columns_incorrect_predictions+=columns_correctly_retrieved
     
-     #columns correctly retrieved son las veces que estan bmi, average glucose level y age en las explicaciones
-    correct_columns=correct_columns+columns_correctly_retrieved
+
+if columns_correctly_retrieved == 3:
+    correct_explanations=correct_explanations+1
     if correct_prediction:
-        counter_columns_correct_predictions+=columns_correctly_retrieved
-        
+        correct_explanations_correct_predictions+=1
     else:
-        counter_columns_incorrect_predictions+=columns_correctly_retrieved
-        
+        correct_explanations_incorrect_predictions+=1
     
-    if columns_correctly_retrieved == 3:
-        correct_explanations=correct_explanations+1
-        if correct_prediction:
-            correct_explanations_correct_predictions+=1
-        else:
-            correct_explanations_incorrect_predictions+=1
-        
-    
-    #if not all(are_columns_present):
-    missing_columns = relevant_columns[np.where(are_columns_present == False)[0]]
-    print("*************************************************************")
-    print('Document id: %d' % i)
-    print('Probability(stroke) =', est.predict_proba([X[i]])[0, 1])
-    print('True class: %s' % class_names[y[i]])
-    if (prediction=='healthy') and (class_names[y[i]]=='healthy'):
-        print('Prediction is correct')
-    elif (prediction=='stroke') and (class_names[y[i]]=='stroke'):
-        print('Prediction is correct')
-    else:
-        print('Prediction is wrong')
-    print(f"Example {i}: missing columns are {','.join(missing_columns)}")
-    print("*************************************************************")
 
-    #fig = exp.as_pyplot_figure()
-    #plt.subplots_adjust(left=0.35)
-    #fig.show()
-    #break
-    
-print('all')
-percentage_total_correct_explanations=correct_explanations/len(dataset)
-percentage_total_correct_columns=correct_columns/(len(dataset)*3)
-print(percentage_total_correct_explanations)   
-print(percentage_total_correct_columns) 
-print('correct prediction')
-print(correct_explanations_correct_predictions/counter_correct_predictions)
-print(counter_columns_correct_predictions/(counter_correct_predictions*3))
-print('incorrect prediction')
-print(correct_explanations_incorrect_predictions/counter_incorrect_predictions)
-print(counter_columns_incorrect_predictions/(counter_incorrect_predictions*3))
+#if not all(are_columns_present):
+missing_columns = relevant_columns[np.where(are_columns_present == False)[0]]
+print("*************************************************************")
+print('Document id: %d' % i)
+print('Probability(stroke) =', est.predict_proba([X[i]])[0, 1])
+print('True class: %s' % class_names[y[i]])
+if (prediction=='healthy') and (class_names[y[i]]=='healthy'):
+    print('Prediction is correct')
+elif (prediction=='stroke') and (class_names[y[i]]=='stroke'):
+    print('Prediction is correct')
+else:
+    print('Prediction is wrong')
+print(f"Example {i}: missing columns are {','.join(missing_columns)}")
+print("*************************************************************")
 
-
-
+fig = exp.as_pyplot_figure()
+#plt.subplots_adjust(left=0.35)
+fig.show()
+#break
+#    
+#print('all')
+#percentage_total_correct_explanations=correct_explanations/len(dataset)
+#percentage_total_correct_columns=correct_columns/(len(dataset)*3)
+#print(percentage_total_correct_explanations)   
+#print(percentage_total_correct_columns) 
+#print('correct prediction')
+#print(correct_explanations_correct_predictions/counter_correct_predictions)
+#print(counter_columns_correct_predictions/(counter_correct_predictions*3))
+#print('incorrect prediction')
+#print(correct_explanations_incorrect_predictions/counter_incorrect_predictions)
+#print(counter_columns_incorrect_predictions/(counter_incorrect_predictions*3))
+#
+#
+#
 
 
 ##encontrar ejemplos de explicaciones razonables en predicciones incorrectas
--- logistic_l1_heart_failure.py
+++ logistic_l1_heart_failure.py
@@ -36,15 +36,16 @@ numerical_idx = X.select_dtypes(include=['float64', 'int64']).columns
 categorical_idx = X.select_dtypes(include=['object', 'bool']).columns
 assert X.shape[1] == (len(numerical_idx) + len(categorical_idx)), 'some column is missing'
 
+scaler=StandardScaler()
 # Encode categorical variables
-X_numerical = StandardScaler().fit_transform(X[numerical_idx])
+X_numerical = scaler.fit_transform(X[numerical_idx])
 oh_enc = OneHotEncoder()
 X_cat = oh_enc.fit_transform(X[categorical_idx]).todense()
 
 X = np.concatenate([X_cat, X_numerical], axis=1)
 X_features = np.concatenate([oh_enc.get_feature_names(), numerical_idx])
 
-C = 0.025
+C = 0.25
 est = LogisticRegression(penalty='l1', C=C, random_state=RANDOM_SEED, solver='saga', max_iter=1000,
                          class_weight='balanced')
 est.fit(X, y)
@@ -86,90 +87,91 @@ prediction='healthy'
 counter_correct_predictions=0
 counter_incorrect_predictions=0
 
-for i in range(len(dataset)):
-    exp = explainer.explain_instance(X[i], est.predict_proba, num_features=2, top_labels=len(X_features))#num features es tres
-    #porque es realemente lo que queremos, lo que se sabemos que son importantes.
-    explanations = [explanation for (explanation, _) in exp.as_list()]
+i=113
+#for i in range(len(dataset)):
+exp = explainer.explain_instance(X[i], est.predict_proba, num_features=11, top_labels=len(X_features))#num features es tres
+#porque es realemente lo que queremos, lo que se sabemos que son importantes.
+explanations = [explanation for (explanation, _) in exp.as_list()]
 
+
+ #prediction de tener un stroke es true (stroke) si su probabilidad es >50%
+
+correct_prediction=True
+if (est.predict_proba([X[i]])[0, 1] ) >= 0.5:
+    prediction='heart_failure'
+else:
+    prediction='healthy'
     
-     #prediction de tener un stroke es true (stroke) si su probabilidad es >50%
-
+if (prediction=='healthy') and (class_names[y[i]]=='healthy'):
     correct_prediction=True
-    if (est.predict_proba([X[i]])[0, 1] ) >= 0.5:
-        prediction='heart_failure'
-    else:
-        prediction='healthy'
-        
-    if (prediction=='healthy') and (class_names[y[i]]=='healthy'):
-        correct_prediction=True
-        counter_correct_predictions=counter_correct_predictions+1
-    elif (prediction=='heart_failure') and (class_names[y[i]]=='heart_failure'):
-        counter_correct_predictions=counter_correct_predictions+1
-        correct_prediction=True
-    else:
-        correct_prediction=False
-        counter_incorrect_predictions=counter_incorrect_predictions+1
+    counter_correct_predictions=counter_correct_predictions+1
+elif (prediction=='heart_failure') and (class_names[y[i]]=='heart_failure'):
+    counter_correct_predictions=counter_correct_predictions+1
+    correct_prediction=True
+else:
+    correct_prediction=False
+    counter_incorrect_predictions=counter_incorrect_predictions+1
 
-    #while (correct_prediction!=True):
-    are_columns_present = []
-    for column_name in relevant_columns:
-            are_columns_present.append(
-                is_column_in_explanations(column_name, explanations)
-            )
-       
+#while (correct_prediction!=True):
+are_columns_present = []
+for column_name in relevant_columns:
+        are_columns_present.append(
+            is_column_in_explanations(column_name, explanations)
+        )
+   
+
+# convert to array to permit vectorial operations
+are_columns_present = np.array(are_columns_present)
+columns_correctly_retrieved = np.sum(are_columns_present)
+
+ #columns correctly retrieved son las veces que estan bmi, average glucose level y age en las explicaciones
+correct_columns=correct_columns+columns_correctly_retrieved
+if correct_prediction:
+    counter_columns_correct_predictions+=columns_correctly_retrieved
     
-    # convert to array to permit vectorial operations
-    are_columns_present = np.array(are_columns_present)
-    columns_correctly_retrieved = np.sum(are_columns_present)
+else:
+    counter_columns_incorrect_predictions+=columns_correctly_retrieved
     
-     #columns correctly retrieved son las veces que estan bmi, average glucose level y age en las explicaciones
-    correct_columns=correct_columns+columns_correctly_retrieved
+
+if columns_correctly_retrieved == 11:
+    correct_explanations=correct_explanations+1
     if correct_prediction:
-        counter_columns_correct_predictions+=columns_correctly_retrieved
-        
+        correct_explanations_correct_predictions+=1
     else:
-        counter_columns_incorrect_predictions+=columns_correctly_retrieved
-        
+        correct_explanations_incorrect_predictions+=1
     
-    if columns_correctly_retrieved == 2:
-        correct_explanations=correct_explanations+1
-        if correct_prediction:
-            correct_explanations_correct_predictions+=1
-        else:
-            correct_explanations_incorrect_predictions+=1
-        
-    
-    #if not all(are_columns_present):
-    missing_columns = relevant_columns[np.where(are_columns_present == False)[0]]
-    print("*************************************************************")
-    print('Document id: %d' % i)
-    print('Probability(heart_failure) =', est.predict_proba([X[i]])[0, 1])
-    print('True class: %s' % class_names[y[i]])
-    if (prediction=='healthy') and (class_names[y[i]]=='healthy'):
-        print('Prediction is correct')
-    elif (prediction=='heart_failure') and (class_names[y[i]]=='heart_failure'):
-        print('Prediction is correct')
-    else:
-        print('Prediction is wrong')
-    print(f"Example {i}: missing columns are {','.join(missing_columns)}")
-    print("*************************************************************")
 
-    #fig = exp.as_pyplot_figure()
-    #plt.subplots_adjust(left=0.35)
-    #fig.show()
+#if not all(are_columns_present):
+missing_columns = relevant_columns[np.where(are_columns_present == False)[0]]
+print("*************************************************************")
+print('Document id: %d' % i)
+print('Probability(heart_failure) =', est.predict_proba([X[i]])[0, 1])
+print('True class: %s' % class_names[y[i]])
+if (prediction=='healthy') and (class_names[y[i]]=='healthy'):
+    print('Prediction is correct')
+elif (prediction=='heart_failure') and (class_names[y[i]]=='heart_failure'):
+    print('Prediction is correct')
+else:
+    print('Prediction is wrong')
+print(f"Example {i}: missing columns are {','.join(missing_columns)}")
+print("*************************************************************")
+
+fig = exp.as_pyplot_figure()
+#plt.subplots_adjust(left=0.35)
+fig.show()
     #break
     
-print('all')
-percentage_total_correct_explanations=correct_explanations/len(dataset)
-percentage_total_correct_columns=correct_columns/(len(dataset)*3)
-print(percentage_total_correct_explanations)   
-print(percentage_total_correct_columns) 
-print('correct prediction')
-print(correct_explanations_correct_predictions/counter_correct_predictions)
-print(counter_columns_correct_predictions/(counter_correct_predictions*3))
-print('incorrect prediction')
-print(correct_explanations_incorrect_predictions/counter_incorrect_predictions)
-print(counter_columns_incorrect_predictions/(counter_incorrect_predictions*3))
+#print('all')
+#percentage_total_correct_explanations=correct_explanations/len(dataset)
+#percentage_total_correct_columns=correct_columns/(len(dataset)*2)
+#print(percentage_total_correct_explanations)   
+#print(percentage_total_correct_columns) 
+#print('correct prediction')
+#print(correct_explanations_correct_predictions/counter_correct_predictions)
+#print(counter_columns_correct_predictions/(counter_correct_predictions*2))
+#print('incorrect prediction')
+#print(correct_explanations_incorrect_predictions/counter_incorrect_predictions)
+#print(counter_columns_incorrect_predictions/(counter_incorrect_predictions*2))
 
 
 
